e2b59cc9a55600928aa96322214afe8325cdcfad
swesh <swesh.mishra123@gmail.com>
2021-11-24 15:47:04 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -12,7 +12,7 @@ import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import users from "../data/users";
+import users from "../users";
 // import image from "./Images/image.jpg";
 import authService from "./../service/authService";
 
